4329c565d24c816997d1dd62e41dc51b0bcc132f
Shaun Hubbard <shaun.hubbard@thoughtworks.com>
2020-03-05 16:03:54 -0600

    simpler for trouble shooting, less simple

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from app import app
 from corona.read_data import read_jh_data
 from corona.plotting import plot
 
-#data = read_jh_data()
+data = read_jh_data()
 
 #raw_html = plot(data, delta=True, raw_html=True)
 
