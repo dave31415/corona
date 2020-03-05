680de40145e3fc899986bea65e8d892c448b5881
Shaun Hubbard <shaun.hubbard@thoughtworks.com>
2020-03-05 15:06:46 -0600

    getting the chart to be served via flask

---
+++ app/routes.py
@@ -0,0 +1,12 @@
+from app import app
+from corona.read_data import read_jh_data
+from corona.plotting import plot
+
+data = read_jh_data()
+
+raw_html = plot(data, delta=True, raw_html=True)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return raw_html
--- corona_module.py
+++ corona_module.py
@@ -1,6 +1,9 @@
 from corona.read_data import read_jh_data
 from corona.plotting import plot
 
+# This script is being used as a scratch pad in order to develop 
+# against the DS package
+
 data = read_jh_data()
 
 raw_html = plot(data, delta=True, raw_html=True)
